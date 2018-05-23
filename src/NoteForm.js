f94e68781c2b8528781d4bd2dbac2eb34182a54c
Ngun Lin Uk <Ngunuk01@gmail.com>
2018-05-23 14:24:06 -0400

    Style NoteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,26 +1,28 @@
 import React from 'react'
 
+import './NoteForm.css'
+
 const NoteForm = () => {
-    return (
-    <div classNsme="NoteForm">
-          <div className="form-actions">
-            <button type="button">
-              <i className="fa fa-trash-o"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-              />
-            </p>
-            
-            <textarea name="body"></textarea>
-          </form>
-        </div>
-    )
+  return (
+    <div className="NoteForm">
+      <div className="form-actions">
+        <button type="button">
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+      <form>
+        <p>
+          <input
+            type="text"
+            name="title"
+            placeholder="Title your note"
+          />
+        </p>
+
+        <textarea name="body"></textarea>
+      </form>
+    </div>
+  )
 }
 
 export default NoteForm
